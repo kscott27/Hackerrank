cc127f908e114043e0e01411e4ed3af2b14491e1
Kevin Scott <kmscott27@gmail.com>
2020-06-16 09:45:42 -0700

    All TCs pass; None time out

--- ArrayManipulation/ArrayManipulation.cpp
+++ ArrayManipulation/ArrayManipulation.cpp
@@ -6,22 +6,67 @@ vector<string> split_string(string);
 
 // Complete the arrayManipulation function below.
 long arrayManipulation(int n, vector<vector<int>> queries) {
-    vector<long> dasArray(n);
+    std::map<long, long> operationMap;
+    operationMap.insert({1, 0});
+
+    int line = 2; // Used for debugging to correspond the iteration with the input line of the current operation
 
     for( auto& row : queries ) {
         int a = row[0];
         int b = row[1];
         int k = row[2];
 
-        for( vector<long>::iterator it = dasArray.begin() + a - 1; it != dasArray.begin() + b; it++ ) {
-            *it = *it + k;
-            cout << *it << endl;
+        // For the starting index, add the delta between this index and the
+        // preceding indexes.
+        auto begin = operationMap.find(a);
+        if( begin != operationMap.end() ) {
+            begin->second = begin->second + k;
+        }
+        else {
+            auto it = operationMap.lower_bound(a);
+            if( it != operationMap.begin() ) {
+                it--; // Decrement to the iterator that holds the value of the range
+                      // our new index is in.
+                operationMap.insert({a, k});
+            }
+            else {
+                cout << "should never get here" << endl;
+            }
+        }
+
+        // Subtract the value of k to signify the
+        // delta between the preceding indexes that received the adding operation,
+        // and the subsequent indexes, that did not.
+        auto subsequent = operationMap.find(b+1);
+        if( subsequent == operationMap.end() ) {
+            auto it = operationMap.lower_bound(b+1);
+            if( b+1 <= n ) {
+                it--;
+                operationMap.insert({b+1, -k}); // k was just added to the previous value, so subtract off to get the subsequent delta.
+            }
+        }
+        else {
+            subsequent->second = subsequent->second - k;
+        }
+
+        // Uncomment for debugging
+        // for( auto&& p : operationMap ) {
+        //     cout << "first: " << p.first << " second: " << p.second << endl;
+        // }
+        // cout << "line: " << line++ << endl;
+    }
+
+    // Sum all of the deltas and cache the maxValue.
+    long maxValue = 0;
+    long rollingValue = 0;
+    for( auto&& op : operationMap ) {
+        rollingValue = rollingValue + op.second;
+        if( rollingValue > maxValue ) {
+            maxValue = rollingValue;
         }
     }
 
-    std::sort(dasArray.begin(), dasArray.end());
-    
-    return dasArray.back();
+    return maxValue;
 }
 
 int main()
